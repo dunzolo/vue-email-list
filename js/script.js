a9d5d77dc8e001724ddb20e70783985f6503d905
dunzolo <rossidavide108@gmail.com>
2023-01-09 14:58:39 +0100

    print emails list

--- js/script.js
+++ js/script.js
@@ -5,7 +5,18 @@ const {
 createApp({
     data() {
         return {
-            
+            array_emails: []
         }
+    },
+    methods: {
+        randomEmail(){
+            this.array_emails = [];
+            for(let i = 0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                    this.array_emails.push(result.data.response);
+                })
+            }
+        }
+        
     },
 }).mount('#app')
